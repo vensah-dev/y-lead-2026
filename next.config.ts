6d0b72f89bac5bdef2809e37b19e88e5a5e47ead
vensah-dev <venkateshdeven14@gmail.com>
2026-06-03 21:43:49 +0800

    pls work

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/y-lead-2026',
+  basePath: isProd ? `/y-lead-2026` : '',
   images: {
     unoptimized: true,
   },
