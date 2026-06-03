b006c3b37582ed991afc89ec1bf0cb26c88b6d04
vensah-dev <venkateshdeven14@gmail.com>
2026-06-03 21:28:41 +0800

    brrrr

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: '/carbide-robotics',
+  basePath: '/y-lead-2026',
   images: {
     unoptimized: true,
   },
