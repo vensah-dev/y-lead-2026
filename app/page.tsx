4e514a45d62befbe0694cd6a5949a40a242c20b5
vensah-dev <venkateshdeven14@gmail.com>
2026-06-29 22:21:33 +0800

    added in instagram icons for instagram content in the about page

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,8 @@ import SplitSection from "@/components/split-section";
 import GlowingImage from "@/components/glowing-image";
 import chairspic from "@/public/images/chairs-pic.png";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-background-primary">
@@ -142,14 +144,22 @@ export default function Home() {
 
               </h2>
 
-              <div className="pt-8 xl:max-w-sm">
+              <div className="pt-8 grid grid-flow-col auto-cols-max gap-2">
                 <PrimaryButton 
                   text={
-                    <>More about the Theme</>
+                    <>More about the theme</>
                   } 
-                  href="/theme" 
+                  href="/about/organising-committee" 
                 />
+
+                <Link href="https://www.instagram.com/p/DZrD5LpD7ZE/?img_index=2" target="_blank" className="flex items-center justify-center aspect-square text-accent-secondary hover:opacity-65 transition-opacity duration-300 scale-75">
+                  <svg className="h-full w-auto" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.051.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
+                  </svg>
+                </Link>
+                
               </div>
+
             </div>
           }
           addClassName="bg-background-secondary py-32 "
@@ -195,14 +205,22 @@ export default function Home() {
                 Lee Boyan, Phoebe Tan En Minn, Ong Yue Yu Valerie, Devendran Venkatesh</p>
               </div>
 
-              <div className="pt-8 xl:max-w-sm">
+              <div className="pt-8 grid grid-flow-col auto-cols-max gap-2">
                 <PrimaryButton 
                   text={
-                    <>More about the organising committee</>
+                    <>More about us</>
                   } 
                   href="/about/organising-committee" 
                 />
+
+                <Link href="https://www.instagram.com/p/DZ61QSevApK/" target="_blank" className="flex items-center justify-center aspect-square text-accent-secondary hover:opacity-65 transition-opacity duration-300 scale-75">
+                  <svg className="h-full w-auto" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.051.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
+                  </svg>
+                </Link>
+
               </div>
+
             </div>
           }
 
