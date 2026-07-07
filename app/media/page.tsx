af6927befffa76b711e4744b37ed4fcaa113385a
vensah-dev <venkateshdeven14@gmail.com>
2026-07-07 14:29:12 +0800

    padlets added

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -35,14 +35,34 @@ export default function Media() {
         {/* <h1 data-animate className="max-w-7xl px-8 mx-auto text-7xl text-center text-font-primary font-display font-bold py-2.5 pb-32">Media</h1> */}
         
         <div className="w-screen bg-background-primary">
-          <div className="md:max-w-7xl w-full px-8 mx-auto py-32">
-            <div className="flex flex-col items-center pb-32">
-              <h1 data-animate className="font-display xl:text-7xl text-6xl max-sm:text-5xl font-bold text-font-primar text-center w-full">
-                Y.LEAD Storyline Video
-              </h1>
+          <div className="md:max-w-7xl w-full px-8 mx-auto py-32 flex flex-col gap-48">
+
+            <div>
+              <div className="flex flex-col items-center pb-24">
+                <h1 data-animate className="font-display xl:text-7xl text-6xl max-sm:text-5xl font-bold text-font-primar text-center w-full">
+                  Y.LEAD Storyline Video
+                </h1>
+              </div>
+              <GlowingEmbed embedUrl="https://www.youtube.com/embed/8c31JGhz_n0?si=QyAT-LpRoO2quYU3" />
             </div>
 
-            <GlowingEmbed embedUrl="https://www.youtube.com/embed/8c31JGhz_n0?si=QyAT-LpRoO2quYU3" />
+            <div>
+              <div className="flex flex-col items-center pb-24">
+                <h1 data-animate className="font-display xl:text-7xl text-6xl max-sm:text-5xl font-bold text-font-primar text-center w-full">
+                  L+E Padlet
+                </h1>
+              </div>
+              <GlowingEmbed embedUrl="https://padlet.com/phoebe_tan_en_minn/l-e-y-lead-2026-collaborative-inquiry-ajb2ixnua615vpkh" />
+            </div>
+
+            <div>
+              <div className="flex flex-col items-center pb-24">
+                <h1 data-animate className="font-display xl:text-7xl text-6xl max-sm:text-5xl font-bold text-font-primar text-center w-full">
+                  A+D Padlet
+                </h1>
+              </div>
+              <GlowingEmbed embedUrl="https://padlet.com/phoebe_tan_en_minn/a-d-y-lead-2026-collaborative-inquiry-i98oa4sxk37p4sru" />
+            </div>
 
             {/* <div className="w-fit mx-auto pt-24">
               <PrimaryButton text="More videos" href="/media"/>
